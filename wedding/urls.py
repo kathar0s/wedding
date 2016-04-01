d02ffc95427dc1d84ad20b4432c5f7579ad8641b
kathar0s <kathar0s@idsntory.com>
2016-04-02 01:26:14 +0900

    스플래시 이미지 업로드

--- wedding/urls.py
+++ wedding/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
-import card
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^card/', include('card.urls', namespace='card')),
     url(r'$', RedirectView.as_view(url='card/', permanent=False))
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
